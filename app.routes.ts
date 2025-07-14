4acbf9fb1531c3490789fb46134ba221060f17c6
Laasya Giri Venkata <97237350+laasyagiri@users.noreply.github.com>
2025-07-14 22:29:54 +0530

    Add files via upload

--- app.routes.ts
+++ app.routes.ts
@@ -4,11 +4,13 @@ import { LandingComponent } from './landingpage/landingpage.component';
 import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { dashboardRoutes } from './dashboard/dashboard.routes';
+import { DebitComponent } from './debit/debit.component';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'debit', component: DebitComponent },
   { path: 'dashboard',
       component: DashboardComponent,
       children: dashboardRoutes
