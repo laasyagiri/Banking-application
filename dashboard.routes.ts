4c5986538eca719380cbe496fd96165cc0c81c5a
Laasya Giri Venkata <97237350+laasyagiri@users.noreply.github.com>
2025-07-14 23:06:47 +0530

    Add files via upload

--- dashboard.routes.ts
+++ dashboard.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from "@angular/router";
 import { LogoutComponent } from "../logout/logout.component";
+import { TransferComponent } from "../transfer/transfer.component";
+import { CreditComponent } from "../credit/credit.component";
+import { DebitComponent } from "../debit/debit.component";
 export const dashboardRoutes:Routes=[
-    
-    {path:'logout',component:LogoutComponent}
+    {path:'transfer', component:TransferComponent},
+    {path:'logout',component:LogoutComponent},
+    {path:'credit', component: CreditComponent},
+    {path:'debit', component: DebitComponent}
 
 ];
