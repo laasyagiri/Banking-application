c5a302b82f87fca279dfe3966ab343a0b7803681
Laasya Giri Venkata <97237350+laasyagiri@users.noreply.github.com>
2025-07-14 22:18:03 +0530

    Add files via upload

--- transfer.component.ts
+++ transfer.component.ts
@@ -1,11 +1,29 @@
+
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-transfer',
-  imports: [],
+  standalone:true,
+  imports:[CommonModule,FormsModule],
   templateUrl: './transfer.component.html',
-  styleUrl: './transfer.component.css'
+  styleUrls: ['./transfer.component.css']
 })
 export class TransferComponent {
+  transferData = {
+    accountNumber: '',
+    confirmAccountNumber: '',
+    accountHolderName: '',
+    amount: null
+  };
 
+  onSubmit() {
+    if (this.transferData.accountNumber !== this.transferData.confirmAccountNumber) {
+      alert('Account numbers do not match!');
+      return;
+    }
+    console.log('Transfer initiated:', this.transferData);
+    alert('Transfer request submitted successfully!');
+  }
 }
