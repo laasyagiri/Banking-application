3e1c3802ce2ec0532392b6c6c4bc76d24825fbfd
Laasya Giri Venkata <97237350+laasyagiri@users.noreply.github.com>
2025-07-14 22:29:20 +0530

    Add files via upload

--- debit.component.ts
+++ debit.component.ts
@@ -1,11 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+ 
 @Component({
   selector: 'app-debit',
-  imports: [],
+  imports:[CommonModule, FormsModule],
   templateUrl: './debit.component.html',
-  styleUrl: './debit.component.css'
+  styleUrls: ['./debit.component.css']
 })
 export class DebitComponent {
-
+  debitModel = {
+    accountNumber: '',
+    name: '',
+    password: '',
+    amount: null
+  };
+ 
+  transactions: any[] = [];
+ 
+  onSubmit(form: any) {
+    if (form.valid) {
+      this.transactions.push({ ...this.debitModel });
+      console.log('Form submitted:', this.debitModel);
+      form.resetForm(); // resets form and model
+    }
+  }
 }
